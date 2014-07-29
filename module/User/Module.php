ba0cb1838b01d92b715c7557cfd76aa9aa1919f1
Duane Jeffers <duane@duanejeffers.com>
2014-07-29 10:43:12 -0500

    Beginning User Module in ZF2 Added User Module to composer.json

---
+++ module/User/Module.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * SMRM - Social Media Relationship Manager
+ * User Module 
+ * 
+ * @author Duane Jeffers <djef@tjc.edu>
+ * @link http://github.com/tylerjuniorcollege/smrm
+ * @copyright Copyright (c) 2014 Tyler Junior College. (http://tjc.edu)
+ * @license MIT - http://github.com/tylerjuniorcollege/smrm/LICENSE
+ **/
+
+namespace User;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
+	public function getConfig() {
+		return include __DIR__ . '/config/module.config.php';
+	}
+}
---
+++ module/User/config/module.config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * SMRM - Social Media Relationship Manager
+ * User Module 
+ * 
+ * @author Duane Jeffers <djef@tjc.edu>
+ * @link http://github.com/tylerjuniorcollege/smrm
+ * @copyright Copyright (c) 2014 Tyler Junior College. (http://tjc.edu)
+ * @license MIT - http://github.com/tylerjuniorcollege/smrm/LICENSE
+ **/
+
+return array(
+	
+);
