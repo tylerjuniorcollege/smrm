3e5135823d734e2db6c6d77445228e78b6088915
Duane Jeffers <duane@duanejeffers.com>
2014-07-29 14:17:05 -0500

    Baseline User Controller created with View Scripts.

---
+++ module/User/src/User/Controller/UserController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * SMRM - Social Media Relationship Manager
+ * User Module 
+ * 
+ * @author Duane Jeffers <djef@tjc.edu>
+ * @link http://github.com/tylerjuniorcollege/smrm
+ * @copyright Copyright (c) 2014 Tyler Junior College. (http://tjc.edu)
+ * @license MIT - http://github.com/tylerjuniorcollege/smrm/LICENSE
+ **/
+
+namespace User\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class UserController extends AbstractActionController {
+	public function indexAction() {
+
+	}
+
+	public function loginAction() {
+
+	}
+
+	public function logoutAction() {
+
+	}
+
+	public function viewAction() {
+
+	}
+
+	public function editAction() {
+
+	}
+
+	public function deleteAction() {
+		
+	}
+
+}
