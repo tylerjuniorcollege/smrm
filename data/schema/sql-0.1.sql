326cc7ef634c0082e6a2799f38b11e676da61b04
Duane Jeffers <duane@duanejeffers.com>
2014-08-01 15:39:54 -0500

    Database setup and Infinite Scroll on the main index.phtml page.

--- data/schema/sql-0.1.sql
+++ data/schema/sql-0.1.sql
@@ -1,13 +1,22 @@
 -- Create Database for SMRM. This line might not be needed for creating the database.
 CREATE DATABASE `smrm`;
 
-CREATE TABLE `users` (
-	`id` int unsigned NOT NULL PRIMARY_KEY AUTO_INCREMENT,
-	`username` varchar(250) NOT NULL,
-	`password` varchar(100) NOT NULL,
-	`firstname` varchar(200) NOT NULL,
-	`lastname` varchar(200) NOT NULL,
-	`email` varchar(250) NOT NULL,
-	`created` datetime NOT NULL
-);
+ CREATE TABLE `user` (
+ 	`userid` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ 	`username` varchar(250) NOT NULL,
+ 	`password` varchar(150) NOT NULL,
+ 	`email` varchar(150) NOT NULL,
+ 	`firstname` varchar(250) NOT NULL,
+ 	`lastname` varchar(250) NOT NULL,
+ 	`created` datetime NOT NULL,
+ 	`updated` datetime NOT NULL
+) COMMENT='' ENGINE='InnoDB';
+
+CREATE TABLE `settings` (
+	`settingid` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`name` varchar(150) NOT NULL,
+	`value` text NOT NULL,
+	`created` datetime NOT NULL,
+	`updated` datetime NOT NULL
+) COMMENT='' ENGINE='InnoDB';
 
