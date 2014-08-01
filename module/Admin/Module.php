43fc5cd4dc555f912bab406fbc375f23520fd930
Duane Jeffers <duane@duanejeffers.com>
2014-08-01 15:39:21 -0500

    Admin Module initial setup

---
+++ module/Admin/Module.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * SMRM - Social Media Relationship Manager
+ * Admin Module 
+ * 
+ * @author Duane Jeffers <djef@tjc.edu>
+ * @link http://github.com/tylerjuniorcollege/smrm
+ * @copyright Copyright (c) 2014 Tyler Junior College. (http://tjc.edu)
+ * @license MIT - http://github.com/tylerjuniorcollege/smrm/LICENSE
+ **/
+
+namespace Admin;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
+	public function getConfig() {
+		return include __DIR__ . '/config/module.config.php';
+	}
+	public function getAutoloaderConfig() {
+		
+	}
+}
